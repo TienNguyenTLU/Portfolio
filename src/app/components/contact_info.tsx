6efc2b02364be8e69e23e0992217e4d2d7e89653
Tien Nguyen <tiencp1234hd@gmail.com>
2025-08-21 19:26:31 +0700

    Add link

--- src/app/components/contact_info.tsx
+++ src/app/components/contact_info.tsx
@@ -3,11 +3,11 @@ import './contact_info.css';
 import { IoIosMail } from 'react-icons/io';
 export default function Contact() {
     const socialLinks = [
-            { name: "GitHub", url: "" ,icon: <FaGithub /> },
-            { name: "LinkedIn", url: "" ,icon: <FaLinkedin /> },
-            { name: "Facebook", url: "",icon: <FaFacebook /> },
-            { name: "Email", url: "mailto:",icon: <IoIosMail /> }
-        ];
+        { name: "GitHub", url: "https://github.com/TienNguyenTLU" ,icon: <FaGithub /> },
+        { name: "LinkedIn", url: "https://www.linkedin.com/in/nguyen-tien-b48495254/" ,icon: <FaLinkedin /> },
+        { name: "Facebook", url: "https://www.facebook.com/tiennguyen394/",icon: <FaFacebook /> },
+        { name: "Email", url: "mailto:tiencp1234hd@gmail.com",icon: <IoIosMail /> }
+    ];
     return (
         <div className="contact-info">
             <h2 className='contact-title'>Contact Information</h2>
