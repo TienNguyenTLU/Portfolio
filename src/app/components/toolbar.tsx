e51563b1b9beb49d794617a88e43fd9c95861bdd
Tien Nguyen <tiencp1234hd@gmail.com>
2025-08-21 18:32:40 +0700

    Fix unescaped entities

--- src/app/components/toolbar.tsx
+++ src/app/components/toolbar.tsx
@@ -5,10 +5,10 @@ import { VscAccount, VscCallIncoming, VscCode, VscHome } from "react-icons/vsc"
 export default function Toolbar()
 {
     const navItems = [
-        { name: "Home", href: "#0" , icon: <VscHome/>},
-        { name: "About", href: "#1" ,icon: <VscAccount/>},
-        { name: "Contact", href: "#3", icon: <VscCallIncoming/> },
-        { name: "Projects", href: "#2", icon: <VscCode/> }
+        {id: 1, name: "Home", href: "#0" , icon: <VscHome/>},
+        {id: 2, name: "About", href: "#1" ,icon: <VscAccount/>},
+        {id:3, name: "Contact", href: "#3", icon: <VscCallIncoming/> },
+        { id:4, name: "Projects", href: "#2", icon: <VscCode/> }
     ]
     return (
         <nav className="toolbar">
@@ -16,7 +16,7 @@ export default function Toolbar()
                 {
                     navItems.map((item) => 
                     (
-                        <li>
+                        <li key={item.id}>
                             <a href={item.href}><span>{item.icon}</span>
                             <span className="tooltip">{item.name}</span></a>
                         </li>
